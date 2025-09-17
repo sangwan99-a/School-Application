db88d3e172a087d716e6f41365b0e94ce16695d0
sangwan99-a <sahilsangwan20011@gmail.com>
2025-09-17 23:05:36 +0530

    fgfg

--- target/generated-sources/annotations/com/school/results/mapper/ResultMapperImpl.java
+++ target/generated-sources/annotations/com/school/results/mapper/ResultMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-17T22:40:30+0530",
+    date = "2025-09-17T23:01:18+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/school/staff/StaffMapperImpl.java
+++ target/generated-sources/annotations/com/school/staff/StaffMapperImpl.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-17T22:40:30+0530",
+    date = "2025-09-17T23:01:19+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 public class StaffMapperImpl implements StaffMapper {
